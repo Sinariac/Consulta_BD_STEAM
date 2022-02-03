00497fc783a9f3c8ed5d2599442d876c6a232401
fernandasoaress <fesoares@id.uff.br>
2022-02-03 17:31:33 -0300

    Adiciona minhas queries

---
+++ Jogos mais jogados e menos jogados por genero/generos.sql
@@ -0,0 +1,8 @@
+/* 5 generos menos jogado */
+
+select nome as jogo, genero, tempo_medio_de_jogo from steam_dados where  tempo_medio_de_jogo > 0 order by  tempo_medio_de_jogo limit 30; 
+
+
+/* 5 generos mais jogado */
+select nome as jogo, genero, tempo_medio_de_jogo from steam_dados order by tempo_medio_de_jogo desc limit 15; 
+
